4fce5fd9cfb84bb875cdca02dd5ba30daadc6fe2
ziyu <qingyang.paul@gmail.com>
2026-08-20 19:05:56 +0800

    Convergence-Measure

--- main.c
+++ main.c
@@ -1,126 +1,63 @@
-#include <ctype.h>
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_INTERVAL_COUNT 200
-#define MAX_NODE_COUNT 64
-#define DEFAULT_MEAN 1000.0
-#define DEFAULT_STDDEV 200.0
+int total_nodes_count = 0;
+int infected_nodes_count = 0;
+int global_round = 0;
+int converged_global_round = -1;
 
-typedef struct {
-    char name;
-    int intervals[MAX_INTERVAL_COUNT];
-    int interval_count;
-    int last_arrival_time;
-    int has_arrival;
-    double mean;
-    double stddev;
-} Node;
-
-static Node nodes[MAX_NODE_COUNT];
-static int node_count = 0;
-
-static int find_node_index(char name) {
-    for (int i = 0; i < node_count; i++) {
-        if (nodes[i].name == name) {
-            return i;
-        }
+void add_global_round(int fanout) {
+    global_round++;
+    int infected_count_before_this_global_round = infected_nodes_count;
+    int uninfected_count_before_this_global_round = total_nodes_count - infected_nodes_count;
+    int potential_newly_infected_nodes_count = fanout * infected_count_before_this_global_round;
+    int newly_infected_nodes_count =
+        potential_newly_infected_nodes_count < uninfected_count_before_this_global_round
+            ? potential_newly_infected_nodes_count
+            : uninfected_count_before_this_global_round;
+    infected_nodes_count += newly_infected_nodes_count;
+    if (infected_nodes_count >= total_nodes_count && converged_global_round == -1) {
+        converged_global_round = global_round;
     }
-    return -1;
 }
 
-static int find_or_create_node_index(char name) {
-    int index = find_node_index(name);
-    if (index >= 0) {
-        return index;
+void add_infected_node(int newly_infected_nodes_count) {
+    infected_nodes_count += newly_infected_nodes_count;
+    if (infected_nodes_count >= total_nodes_count && converged_global_round == -1) {
+        converged_global_round = global_round;
     }
-
-    if (node_count == MAX_NODE_COUNT) {
-        return -1;
-    }
-
-    nodes[node_count] = (Node){
-        .name = name,
-        .mean = DEFAULT_MEAN,
-        .stddev = DEFAULT_STDDEV,
-    };
-    return node_count++;
 }
 
-static void update_interval_stats(Node *node) {
-    if (node->interval_count == 0) {
-        node->mean = DEFAULT_MEAN;
-        node->stddev = DEFAULT_STDDEV;
-        return;
-    }
-
-    double sum = 0.0;
-    for (int i = 0; i < node->interval_count; i++) {
-        sum += node->intervals[i];
-    }
-    node->mean = sum / node->interval_count;
-
-    double squared_difference_sum = 0.0;
-    for (int i = 0; i < node->interval_count; i++) {
-        double difference = node->intervals[i] - node->mean;
-        squared_difference_sum += difference * difference;
-    }
-    node->stddev = sqrt(squared_difference_sum / node->interval_count);
-}
-
-static void add_heartbeat(char name, int arrival_time) {
-    int index = find_or_create_node_index(name);
-    if (index < 0) {
-        return;
-    }
-
-    Node *node = &nodes[index];
-    if (node->has_arrival) {
-        int interval = arrival_time - node->last_arrival_time;
-        if (node->interval_count < MAX_INTERVAL_COUNT) {
-            node->intervals[node->interval_count++] = interval;
-        }
-        update_interval_stats(node);
-    }
-
-    node->last_arrival_time = arrival_time;
-    node->has_arrival = 1;
-}
-
-static double calculate_phi(char name, int current_time) {
-    int index = find_node_index(name);
-    if (index < 0 || !nodes[index].has_arrival) {
-        return 0.0;
-    }
-
-    Node *node = &nodes[index];
-    double elapsed = current_time - node->last_arrival_time;
-    if (node->stddev == 0.0) {
-        return 0.0;
-    }
-    return (elapsed - node->mean) / node->stddev;
+int get_converged_global_round(void) {
+    return converged_global_round;
 }
 
 int main(void) {
     char line[1024];
-
     while (fgets(line, sizeof line, stdin)) {
-        char command[16];
-        char name;
-        int timestamp;
-
-        if (sscanf(line, "%15s %c %d", command, &name, &timestamp) != 3) {
+        if (line[0] == '\n' || line[0] == 0) {
             continue;
         }
 
-        if (strcmp(command, "HEARTBEAT") == 0) {
-            add_heartbeat(name, timestamp);
-        } else if (strcmp(command, "PHI") == 0) {
-            printf("phi=%.2f\n", calculate_phi(name, timestamp));
+        if (strncmp(line, "CONVERGED?", 10) == 0) {
+            int converged_global_round = get_converged_global_round();
+            if (converged_global_round == -1) {
+                printf("NO\n");
+            } else {
+                printf("YES %d\n", converged_global_round);
+            }
+        } else if (strncmp(line, "INIT ", 5) == 0) {
+            total_nodes_count = atoi(line + 5);
+            infected_nodes_count = 0;
+            global_round = 0;
+            converged_global_round = -1;
+        } else if (strncmp(line, "INFECT ", 7) == 0) {
+            add_infected_node(atoi(line + 7));
+        } else if (strncmp(line, "ROUND ", 6) == 0) {
+            int fanout = atoi(line + 6);
+            add_global_round(fanout);
         }
     }
-
     return 0;
 }
